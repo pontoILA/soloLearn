1f4a4780b99ab4ce50ff396e8d8b464caea669ca
Daniel Marco <danielmarco49@gmail.com>
2021-08-07 20:26:10 -0300

    firstProgramC#

---
+++ C#/FisrtProgram.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output "C# is cool"
+            Console.WriteLine("C# is cool");
+            
+        }
+    }
+}
+
